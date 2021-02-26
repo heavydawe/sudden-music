4177a05871cbc6848fa8b034f2c6119a318afc37
heavydawe <47754956+heavydawe@users.noreply.github.com>
2021-02-26 20:18:56 +0100

    Test synths and timing

---
+++ src/AppComponents/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Footer.css'
+
+function Footer() {
+  return (
+    <footer>
+      <button>Log in</button>
+      <button>Sign up</button>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/AppComponents/PianoRollView/PianoRollView.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PianoRollView() {
+  return <div></div>;
+}
+
+export default PianoRollView;
